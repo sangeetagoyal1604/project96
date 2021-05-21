cd563d12e6bc132cb04e2ff828732a9181eddc00
sangeetagoyal1604 <80881704+sangeetagoyal1604@users.noreply.github.com>
2021-05-21 21:57:39 +0530

    Add files via upload

---
+++ Lets chat_page.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqDUHcZ3fkCl3FaPPinaL3rjL4dp4vGbk",
+    authDomain: "chatapp-for-school.firebaseapp.com",
+    databaseURL: "https://chatapp-for-school-default-rtdb.firebaseio.com",
+    projectId: "chatapp-for-school",
+    storageBucket: "chatapp-for-school.appspot.com",
+    messagingSenderId: "673546131426",
+    appId: "1:673546131426:web:b531a6b635d76a6c170bbb"
+  };
